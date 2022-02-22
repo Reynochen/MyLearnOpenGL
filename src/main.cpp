bf94057ca36242bf91562a1e04476aeb2ebed925
Reych <justnikitaruda@gmail.com>
2022-02-22 17:53:30 +0300

    Mini testing OpenGL/GLFW

--- src/main.cpp
+++ src/main.cpp
@@ -2,43 +2,60 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+int winSizeX = 640;
+int winSizeY = 480;
+
+void glfwWindowSizeCallBack(GLFWwindow* pWindow, int width, int height)
+{
+    winSizeX = width;
+    winSizeY = height;
+    glViewport(0, 0, winSizeX, winSizeY);
+    std::cout << "You sus\n";
+}
+
+void glfwKeyCallBack(GLFWwindow* pWindow, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(pWindow, true);
+}
+
 int main(void)
 {
-    GLFWwindow* window;
-
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    GLFWwindow* pWindow = glfwCreateWindow(winSizeX , winSizeY, "Hello World", NULL, NULL);
+    if (!pWindow) {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    glfwSetWindowSizeCallback(pWindow, glfwWindowSizeCallBack);
+    glfwSetKeyCallback(pWindow, glfwKeyCallBack);
 
-    if (!gladLoadGL())
-    {
+    /* Make the window's context current */
+    glfwMakeContextCurrent(pWindow);
+
+    if (!gladLoadGL()) {
         std::cout << "Glad not loaded";
         return -1;
     }
     
-    std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
+    std::cout << "Render: " << glGetString(GL_RENDERER) << '\n';
+    std::cout << "OpenGL " << glGetString(GL_VERSION) << '\n';
 
     glClearColor(0, 0, 1, 0);
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(pWindow))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(pWindow);
 
         /* Poll for and process events */
         glfwPollEvents();
